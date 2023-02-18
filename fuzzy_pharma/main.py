61f1e9a884e04b6da660e680d511fc140b479ec9
Aleksey Voko <voko.aleksey@gmail.com>
2023-02-18 12:07:11 +0400

    feat(fuzzy_pharma): add 'get_data_frames()'

--- fuzzy_pharma/main.py
+++ fuzzy_pharma/main.py
@@ -1,5 +1,22 @@
+import pandas as pd
+
+from fuzzy_pharma.definitions import NOMENCLATURE
+
+
+def get_data_frames(nomenclature_dir, header=None):
+    nomenclature_files = nomenclature_dir.glob('*')
+
+    df = pd.DataFrame()
+
+    for nomenclature in nomenclature_files:
+        n = pd.read_excel(nomenclature, header=header)
+        df = pd.concat([df, n], ignore_index=True, axis=0)
+
+    return df
+
+
 def main():
-    pass
+    nomenclature_df = get_data_frames(NOMENCLATURE)
 
 
 if __name__ == '__main__':
