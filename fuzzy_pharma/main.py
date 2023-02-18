add440998e59c3c997913db1216fc08156b79749
Aleksey Voko <voko.aleksey@gmail.com>
2023-02-18 13:39:39 +0400

    feat(fuzzy_pharma): add 'get_client_data_frames()'

--- fuzzy_pharma/main.py
+++ fuzzy_pharma/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from fuzzy_pharma.definitions import NOMENCLATURE
+from fuzzy_pharma.definitions import NOMENCLATURE, CLIENTS
 
 
 def get_data_frames(nomenclature_dir, header=None):
@@ -15,8 +15,21 @@ def get_data_frames(nomenclature_dir, header=None):
     return df
 
 
+def get_client_data_frames(client_dir, usecols, header=0):
+    client_files = client_dir.glob('*')
+
+    df = pd.DataFrame()
+
+    for client in client_files:
+        c = pd.read_excel(client, header=header, usecols=usecols)
+        df = pd.concat([df, c], ignore_index=True, axis=0)
+
+    return df
+
+
 def main():
     nomenclature_df = get_data_frames(NOMENCLATURE)
+    clients_df = get_client_data_frames(CLIENTS, ['наименование'])
 
 
 if __name__ == '__main__':
