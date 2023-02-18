34dcd7adfa81b6bbd31ca293459109aaea0c57b5
Aleksey Voko <voko.aleksey@gmail.com>
2023-02-18 10:57:47 +0400

    feat(fuzzy_pharma): add 'definitions.py'

---
+++ fuzzy_pharma/definitions.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+ROOT_DIR = Path(__file__).parent.parent
+DATA_IN = ROOT_DIR / 'data_in'
+
+CLIENTS = DATA_IN / 'clients'
+NOMENCLATURE = DATA_IN / 'nomenclature'
+
+DATA_OUT = ROOT_DIR / 'data_out'
